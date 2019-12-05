ae73448f23f9ca886ff1d0e197707eb7db07b688
Dmytro Lutfulin <dmytro.lutfulin@nure.ua>
2019-12-05 14:06:34 +0200

    add lab3

---
+++ dmytro.lutfulin-master/src/main/java/ua/nure/kn/lutfulin/domain/db/UserDao.java
@@ -0,0 +1,22 @@
+package ua.nure.kn.lutfulin.domain.db;
+
+import java.util.Collection;
+
+import ua.nure.kn.panasiuk.domain.User;
+
+public interface UserDao {
+	
+	User create(User user) throws DatabaseException;
+	
+	void update(User user) throws DatabaseException;
+
+    void delete(User user) throws DatabaseException;
+
+    User find(Long ID) throws DatabaseException;
+
+    Collection <User> findAll() throws DatabaseException;
+
+    void setConnectionFactory(ConnectionFactory connectionFactory);
+
+}
+	
